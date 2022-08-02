a9be5390fda0e32a1ebc73be0af79acf46c7532d
Lucas Sonsin Lima <lucassonsin.lima@gmail.com>
2022-08-01 23:27:16 -0300

    feat: add nitro config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  target: 'static',
+  nitro: {
+    commands: {
+      preview: "npx serve ./public",
+    },
+    serveStatic: true,
+    prerender: {
+      routes: ['/get-started']
+    }
+  }
 })
