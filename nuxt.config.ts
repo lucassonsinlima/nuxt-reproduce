667b779615a3979550d73475ecfcce5e4a5299e6
Lucas Sonsin Lima <lucassonsin.lima@gmail.com>
2022-09-19 11:44:18 -0300

    refactor: cleanup all unused configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,12 @@
 import { defineNuxtConfig } from 'nuxt'
 
-const routes = ['/', '/get-started', '/schedule', '/sourcing', '/404'];
+const routes = ['/404'];
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: 'static',
-  ssr: true,
-  generate: {
-    routes,
-    fallback: '404/index.html',
-    subFolders: false
-  },
-  router: {
-    trailingSlash: true,
-    strict: true
-  },
   nitro: {
+    prerender: { routes },
     commands: {
       preview: "npx serve ./public",
     }
